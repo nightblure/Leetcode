9d56a337758748f2966a2c32db736570aa8677a9
unknown <vanobel159@gmail.com>
2023-08-01 14:32:46 +0300

    Refactoring of number of islands

--- Problems Solutions/200. Number of Islands.py
+++ Problems Solutions/200. Number of Islands.py
@@ -3,30 +3,25 @@
 class Solution:
     def numIslands(self, grid) -> int:
 
-        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
         def mark_island(i, j, grid, n, m):
-            if i < 0 or i > n - 1 or j < 0 or j > m - 1:
-                return
+            grid[i][j] = "#"
+            directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-            if grid[i][j] != '1':
-                return
+            for dir in directions:
+                x, y = i + dir[0], j + dir[1]
 
-            grid[i][j] = '#'
-
-            for direction in directions:
-                mark_island(i + direction[0], j + direction[1], grid, n, m)
-            
-            return grid
+                if x in range(0, n) and y in range(0, m) and grid[x][y] == "1":
+                    mark_island(x, y, grid, n, m)
         
         n, m = len(grid), len(grid[0])
         count = 0
 
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == '1':
-                    grid = mark_island(i, j, grid, n, m)
+                if grid[i][j] == "1":
+                    mark_island(i, j, grid, n, m)
                     count += 1
+
         return count
 
 tests_data = (

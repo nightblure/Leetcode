ed67ea7faf99bab4c6e9c9457de0153e6d6ac2e7
Ivan <vanobel159@gmail.com>
2025-01-26 20:26:49 +0300

    Update 141. Linked List Cycle.py

--- Problems Solutions/141. Linked List Cycle.py
+++ Problems Solutions/141. Linked List Cycle.py
@@ -4,14 +4,13 @@ from utils import list_to_linked_list, ListNode, is_same_linked_lists
 
 class Solution:
     def hasCycle(self, head) -> bool:
-        dummy = ListNode()
-        dummy.next = head
-        slow, fast = dummy, dummy.next
-
+        slow, fast = head, head
+        
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
+
+            if slow is fast:
                 return True
-        
+
         return False
